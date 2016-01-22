b69797bfbcb4871964a3b4cf3f907ec2582f44f4
Steve Desmond <steve@vtsv.ca>
2016-01-22 15:45:51 -0500

    fix live switcher to work with HSTS

--- LiveSwitcher/Program.cs
+++ LiveSwitcher/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Xml.Linq;
 
 namespace LiveSwitcher
@@ -8,16 +9,20 @@ namespace LiveSwitcher
         public static void Main(string[] args)
         {
             var xml = XDocument.Load("web.config");
-            var url = xml.Root?.Element("system.webServer")?.Element("rewrite")?.Element("rules")?.Element("rule")?.Element("action")?.Attribute("url");
-            if (url != null)
+            var root = xml.Root;
+            var server = root?.Element("system.webServer");
+            var rewrite = server?.Element("rewrite");
+            var rules = rewrite?.Element("rules");
+            var rule = rules?.Elements("rule").First(r => r.Attribute("name").Value == "Reverse Proxy");
+            var action = rule?.Element("action");
+            var url = action?.Attribute("url");
+            if (url == null) return;
+            if (args.Length >= 1)
             {
-                if (args.Length >= 1)
-                {
-                    url.SetValue("http://127.0.0.1:" + args[0] + "/{R:1}");
-                    xml.Save("web.config");
-                }
-                Console.WriteLine(url.Value);
+                url.SetValue("http://127.0.0.1:" + args[0] + "/{R:1}");
+                xml.Save("web.config");
             }
+            Console.WriteLine(url.Value);
         }
     }
 }
