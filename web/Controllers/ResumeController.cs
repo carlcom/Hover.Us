f85a9d54295fa9b6fe2f890146a2dcbf33067769
Steve Desmond <steve@vtsv.ca>
2016-01-23 16:16:05 -0500

    front-end display fixes after HTML5ification

--- web/Controllers/ResumeController.cs
+++ web/Controllers/ResumeController.cs
@@ -16,22 +16,23 @@ namespace Web.Controllers
                 var active = System.IO.File.ReadAllLines(Path.Combine("D:\\", "www", "active.txt"));
                 if (active.Contains(For))
                 {
-                    var contactPage = Cache.Pages.FirstOrDefault(p => p.Category.Matches("Resume") && p.URL.Matches("Contact"));
+                    var contactPage = Cache.Pages.First(p => p.Category.Matches("Resume") && p.URL.Matches("Contact"));
                     ViewBag.ContactInfo = contactPage.Body;
                 }
             }
-            var resume = Cache.Pages.FirstOrDefault(p => p.Category.Matches("Resume") && p.URL.Matches("Resume"));
+            var resume = Cache.Pages.First(p => p.Category.Matches("Resume") && p.URL.Matches("Resume"));
             ViewBag.Subtitle = resume.Title;
+            ViewBag.NoFooter = true;
             return View("Index", resume);
         }
 
         public IActionResult Contact()
         {
-            var lolPage = Cache.Pages.FirstOrDefault(p => p.Category.Matches("Resume") && p.URL.Matches("LOL"));
+            var lolPage = Cache.Pages.First(p => p.Category.Matches("Resume") && p.URL.Matches("LOL"));
             var niceTry = new ContentResult
             {
                 ContentType = new MediaTypeHeaderValue("text/plain"),
-                Content = lolPage.Body.ToString()
+                Content = lolPage.Body
             };
             return niceTry;
         }
