50e720c70b70b968d3da6c14c64894f09d19bd60
Steve Desmond <steve@vtsv.ca>
2016-07-02 22:45:25 -0400

    add Dockerfile

--- Web/Models/Settings.cs
+++ Web/Models/Settings.cs
@@ -6,7 +6,7 @@
         public const string Domain = "https://stevedesmond.ca/";
         public const string Description = "Independent contractor and software consultant based in Ithaca, NY; working remotely with teams and businesses everywhere";
         public const string ImageBase = Domain + "images";
-        public const string ConnectionString = @"Data Source=C:\www\db.sqlite";
+        public const string ConnectionString = @"Data Source=D:\www\db.sqlite";
         public const string EmailFromAndTo = "hi@stevedesmond.ca";
         public const string EmailServer = "smtp.googlemail.com";
         public const string EmailUser = "steve@vtsv.ca";
