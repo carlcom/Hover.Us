6a7950760e4187422065f04974ab11553b238248
Steve Desmond <steve@stevedesmond.ca>
2016-11-11 11:21:18 -0500

    JSON-LD validation

--- Web/Models/Settings.cs
+++ Web/Models/Settings.cs
@@ -13,5 +13,36 @@
         public const string EmailServer = "smtp.googlemail.com";
         public const string EmailUser = "steve@vtsv.ca";
         public const string EmailSubject = "Contact Form Submission";
+
+        public const string OrgJSON =
+        @"{
+            ""@context"": ""http://schema.org"",
+            ""@type"": ""Organization"",
+            ""@id"": """ + Domain + @""",
+            ""name"": """ + Title + @""",
+            ""url"": """ + Domain + @""",
+            ""image"": " + ProfileImageJSON + @",
+            ""logo"": " + ProfileImageJSON + @",
+            ""description"": """ + Description + @""",
+            ""mainEntityOfPage"": """ + Domain + @""",
+            ""email"": """ + EmailFromAndTo + @""",
+            ""legalName"": """ + Title + @""",
+            ""location"": {
+                ""@context"": ""http://schema.org"",
+                ""@type"": ""PostalAddress"",
+                ""addressLocality"": ""Ithaca"",
+                ""addressRegion"": ""NY"",
+                ""addressCountry"": ""US""
+            }
+        }";
+
+        public const string ProfileImageJSON =
+        @"{
+            ""@context"": ""http://schema.org"",
+            ""@type"": ""ImageObject"",
+            ""url"": ""https://stevedesmond.ca/images/steve-0300.jpg"",
+            ""height"": ""300"",
+            ""width"": ""300""
+        }";
     }
 }
